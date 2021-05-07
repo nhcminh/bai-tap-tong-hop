1eabfe5110855b4e8dd1b0fc74536aba51be9052
unknown <a2mrken@gmail.com>
2021-05-08 02:41:05 +0700

    done: layout css home -> services + js navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-$('.owl-carousel').owlCarousel({
-    nav:true,
-    loop: true,
-    dots:true,
-    items: 1,
-    margin: 0,
-    navText: ['',''],
+$(document).ready(function () {
+    // Owl Init
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        loop: true,
+        dots: true,
+        items: 1,
+        margin: 0,
+        navText: ['', ''],
+    })
+
+    // Navbar
+    $(document).on('scroll', function(){
+        if(pageYOffset != 0){
+            $('.nav_brand').css({'width':'40%'});
+            $('#navbar').css({'background-color': 'rgba(255, 255, 255, 1)'});
+        }
+        else {
+            $('.nav_brand').css({'width':'65%'});
+            $('#navbar').css({'background-color': 'rgba(255, 255, 255, 0)'});
+        }
+    })
 })

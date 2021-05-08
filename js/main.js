3784c31e38939308d81beb8c3a1c02f6ee417057
unknown <anhnhattran97@gmail.com>
2021-05-09 04:03:35 +0700

    Hoàn thành đến section purchase

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
 $(document).ready(function () {
-    // Owl Init
-    $('.owl-carousel').owlCarousel({
-        autoplay:true,
-        nav: true,
-        loop: true,
-        dots: true,
-        items: 1,
-        margin: 0,
-        navText: ['', ''],
-    })
+  // Owl Init
+  $(".owl-carousel").owlCarousel({
+    // autoplay: true,
+    // nav: true,
+    loop: true,
+    dots: true,
+    items: 1,
+    margin: 0,
+    navText: ["", ""],
+  });
 
-    // Navbar
-    $(document).on('scroll', function(){
-        if(pageYOffset != 0){
-            $('.nav_brand').css({'width':'62px'});
-            $('#navbar').css({'background-color': 'rgba(255, 255, 255, 1)'});
+  $('.owl-2').owlCarousel({
+    loop:true,
+    autoplay: false,
+    dots:true,
+    nav:false ,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
         }
-        else {
-            $('.nav_brand').css({'width':'105px'});
-            $('#navbar').css({'background-color': 'rgba(255, 255, 255, 0)'});
-        }
-    })
+    }
 })
+  // Navbar
+
+  $(document).on("scroll", function () {
+    if (pageYOffset != 0) {
+      $(".nav_brand").css({ width: "62px" });
+      $("#navbar").css({ "background-color": "rgba(255, 255, 255, 1)" });
+    } else {
+      $(".nav_brand").css({ width: "105px" });
+      $("#navbar").css({ "background-color": "rgba(255, 255, 255, 0)" });
+    }
+  });
+});
